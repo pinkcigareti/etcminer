a0ae37ec63fccfa85a26d21c8a212417b6b6492e
Jean Cyr <jean.m.cyr@gmail.com>
2021-01-13 01:11:18 -0500

    - create release .zip file for windows

--- winrelease.bat
+++ winrelease.bat
@@ -8,4 +8,4 @@ setx CUDA_TOOLKIT_ROOT_DIR "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v
 cmake .. -G "Visual Studio 15 2017 Win64" -DETHASHCL=ON -DAPICORE=ON -DBINKERN=OFF -DETHASHCUDA=ON -T v140
 cmake --build . --config Release
 cd ..
-copy build\ethminer\Release\ethminer.exe release
+"c:\Program Files\WinRAR\WinRAR.exe" a -afzip -ep1 release\win10-ethminer.zip build\ethminer\Release\ethminer.exe
