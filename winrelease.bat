e34a6647dcc11e9cf8aaa3e290c0f9814197401d
Jean Cyr <jean.m.cyr@gmail.com>
2021-01-16 18:22:05 -0500

    - fix release builds

--- winrelease.bat
+++ winrelease.bat
@@ -4,8 +4,8 @@ mkdir release
 mkdir build
 cd build
 
-setx CUDA_TOOLKIT_ROOT_DIR "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v20.0"
+setx CUDA_TOOLKIT_ROOT_DIR "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.0"
 cmake .. -G "Visual Studio 15 2017 Win64" -DETHASHCL=ON -DAPICORE=ON -DBINKERN=OFF -DETHASHCUDA=ON -T v140
 cmake --build . --config Release
 cd ..
-"c:\Program Files\WinRAR\WinRAR.exe" a -afzip -ep1 release\win10-ethminer.zip build\ethminer\Release\ethminer.exe
+"c:\Program Files\WinRAR\WinRAR.exe" a -afzip -ep1 release\win-nsfminer.zip build\nsfminer\Release\nsfminer.exe
