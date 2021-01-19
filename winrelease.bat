c401b56b48602604448116df73c5a9d7622137b8
Jean M. Cyr <no.fee.ethereum.ming@gmail.com>
2021-01-19 17:21:17 -0500

    - support deprecated Nvidia architecture - Update release scripts

--- winrelease.bat
+++ winrelease.bat
@@ -8,4 +8,4 @@ setx CUDA_TOOLKIT_ROOT_DIR "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v
 cmake .. -G "Visual Studio 15 2017 Win64" -DETHASHCL=ON -DAPICORE=ON -DBINKERN=OFF -DETHASHCUDA=ON -T v140
 cmake --build . --config Release
 cd ..
-"c:\Program Files\WinRAR\WinRAR.exe" a -afzip -ep1 release\win-nsfminer.zip build\nsfminer\Release\nsfminer.exe
+"c:\Program Files\WinRAR\WinRAR.exe" a -afzip -ep1 release\win-nsfminer-cuda11-0.0.0.zip build\nsfminer\Release\nsfminer.exe
